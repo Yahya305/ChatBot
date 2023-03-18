bd77aacfbed1244116a6e5ffc65c291842d5edf1
Yahya305 <saimyahya47@gmail.com>
2023-03-18 21:41:11 +0500

    Update App.js

--- ubit-web-main/src/App.js
+++ ubit-web-main/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
